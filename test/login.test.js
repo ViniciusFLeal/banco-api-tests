51d903c4b52ecaf0e5ba9c573b83bb7455d0d140
ViniciusFleal <viniciusfleal@gmail.com>
2025-10-25 17:44:11 -0300

    Adicionando testes de transferência

--- test/login.test.js
+++ test/login.test.js
@@ -12,8 +12,7 @@ describe('Login', () => {
                     'senha': '123456'
                 });
             
-                console.log(resposta.status)
-                console.log(resposta.body)
+                
 
             expect(resposta.status).to.equal(200);
             expect(resposta.body.token).to.be.a('string');
